7012900fa5844ebd6aae8761d04ef322857720e5
Yaroslav-Korzh <dunny04@gmail.com>
2023-12-21 18:28:49 +0100

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 4321;
+        int reverse = 0;
+        int remainder;
+
+        for (; x != 0; x = x/10) {
+            remainder = x % 10;
+            reverse = reverse * 10 + remainder;
+
+        }
+        System.out.println(reverse);
     }
 }
